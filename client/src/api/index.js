034ff2b41e57c9f380308e6b6c62989d3277c014
haythema19 <duongpc99@gmail.com>
2023-01-24 21:51:39 +0700

    Update index.js

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const baseURL = "http://localhost:4000/";
+const baseURL = "https://musicappapi.vercel.app/";
 
 export const validateUser = async (token) => {
   try {
